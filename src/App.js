14eb563b7d812c425a3e75a61003f3a258ab84d1
ademulic <ademulic@hotmail.com>
2024-10-29 23:19:54 +0100

    Weather App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
+ 
 function App() {
+  let [temperature, setTemperature]= useState(0);
+  const [feeling, setFeeling] = useState('');
+  const [background, setBackground] = useState('orange');
+  const [fontColor, setFontColor] = useState('white');
+  function getRandomNumber(min, max) {
+    return Math.ceil(Math.random() * (max - min) + min);
+  }
+
+  const showWeather=()=>{ 
+    temperature = getRandomNumber(-100,40)
+    setTemperature(temperature);
+
+    if(temperature<-4 ){ 
+      setFeeling("brrr, it's freezing!!!");
+      setBackground('lightblue');
+      setFontColor('slateblue');
+    }
+    else if(temperature >= -4 && temperature<=22){ 
+      setFeeling('');
+      setBackground('orange');
+      setFontColor('white');
+    }else if(temperature>22){ 
+      setFeeling("It's really warm");
+      setBackground('red');
+      setFontColor('yellow'); 
+    } 
+   
+   
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{
+        display:'flex',
+        flexDirection:'column',
+        alignItems:'center',
+        justifyContent:"space-around",
+        width:'80vw',
+        height:'80vh',
+        border:'1px solid black',
+        backgroundColor:background,
+        color:fontColor
+      }}>
+        <h1>{temperature}</h1>
+        <p>{feeling}</p>
+        <button onClick={showWeather}>Change Temperature</button>
+      </div>
     </div>
   );
 }
